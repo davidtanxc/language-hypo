f54f66e257008b4954f7c6bb018596b3cd576181
Elisha <elisha@learnemy.com>
2014-06-09 18:01:44 +0800

    New app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LanguageHypothesis::Application.config.secret_key_base = '12ac218a816015b0159fc48453385d2a50dcd57fdb11e0c3f9be01a6b00938a81f7139dd84a5c1923bb58053e1f0b9cf9330f3d761f0e39dfde1a0aa86ef3f75'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LanguageHypothesis::Application.config.session_store :cookie_store, key: '_language-hypothesis_session'
