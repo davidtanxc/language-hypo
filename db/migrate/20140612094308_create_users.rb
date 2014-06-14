c5c17fb110c88eaca8aa7c1fb0c28b88a337e39c
Elisha <elisha@learnemy.com>
2014-06-14 15:04:31 +0800

    Landing page frame

--- db/migrate/20140612094308_create_users.rb
+++ db/migrate/20140612094308_create_users.rb
@@ -1,8 +1,14 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-
+      t.string :full_name
+      t.string :email
+      t.string :phone_number
+      t.string :topic
+      t.string :class_type
       t.timestamps
     end
   end
 end
+
+
