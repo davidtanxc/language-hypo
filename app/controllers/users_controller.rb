a20473239a6ac60970fca5ac48ad4ce4c9f38747
Elisha <elisha@learnemy.com>
2014-06-16 18:55:04 +0800

    touch up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -70,6 +70,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:full_name, :email, :phone_number, :class_types => [], :topics => [])
+      params.require(:user).permit(:full_name, :email, :phone_number, :topic)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,31 +1,31 @@
 class User < ActiveRecord::Base
 
-  TOPICS = ["Tennis", "Squash", "Table Tennis", "Badminton", "Bowling" ]
-  CLASSTYPES = ["Private", "Group" ]
+  # TOPICS = ["Tennis", "Squash", "Table Tennis", "Badminton", "Bowling" ]
+  # CLASSTYPES = ["Private", "Group" ]
 
-  attr_accessor :topics, :classtypes
+  # attr_accessor :topics, :classtypes
 
-  def topic_selectors
-    TOPICS.collect { |topic| [(self.topic || "").include?(topic), topic] }
-  end
+  # def topic_selectors
+  #   TOPICS.collect { |topic| [(self.topic || "").include?(topic), topic] }
+  # end
 
-  def topics=(value)
-    self.topic = value.join(", ")
-  end
+  # def topics=(value)
+  #   self.topic = value.join(", ")
+  # end
 
-  def topics
-    (self.topic || "").split(", ")
-  end
+  # def topics
+  #   (self.topic || "").split(", ")
+  # end
 
-  def type_selectors
-    CLASSTYPES.collect { |class_type| [(self.class_type || "").include?(class_type), class_type] }
-  end
+  # def type_selectors
+  #   CLASSTYPES.collect { |class_type| [(self.class_type || "").include?(class_type), class_type] }
+  # end
 
-  def class_types=(value)
-    self.class_type = value.join(", ")
-  end
+  # def class_types=(value)
+  #   self.class_type = value.join(", ")
+  # end
 
-  def class_types
-    (self.class_type || "").split(", ")
-  end
+  # def class_types
+  #   (self.class_type || "").split(", ")
+  # end
 end
