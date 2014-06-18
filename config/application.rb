141772f8d318052eba3174740c64dd0e2ba6582b
Elisha <elisha@learnemy.com>
2014-06-18 15:57:14 +0800

    Done emails

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(:default, Rails.env)
 
 module LanguageHypothesis
   class Application < Rails::Application
+
+    config.time_zone = 'Singapore'
+    config.active_record.default_timezone = 'Singapore'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
