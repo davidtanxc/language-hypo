90f8b82c78dc612bc548554168c60cd0f5b942aa
Elisha <elisha@learnemy.com>
2014-06-23 20:36:28 +0800

    Change email

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -3,12 +3,12 @@ class Notifier < ActionMailer::Base
 
   def new_user(user)
     @user = user
-    mail(:to => "team@learnemy.com", :subject => "New user")
+    mail(:to => "mingkwang@learnemy.com", :subject => "New user")
   end
 
   def new_order(order)
     @order = order
-    mail(:to => "team@learnemy.com", :subject => "New order")
+    mail(:to => "mingkwang@learnemy.com", :subject => "New order")
   end
 
 end
